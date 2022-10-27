5d434365c8299dd5c9fc8f44f6f45c8a9bdc7b95
sasaki <s21c1053mt@s.chibakoudai.jp>
2022-10-27 12:31:44 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-seq 5 | ./plus
+ng() {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+
+out=$(seq 5 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+
+exit $res
