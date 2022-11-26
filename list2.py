fb5518fc3741f92774202ce4810525258a748e38
sasaki <s21c1053mt@s.chibakoudai.jp>
2022-11-26 16:21:57 +0900

    Add a command

--- list2.py
+++ list2.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2022 Yuto Sasaki
+# SPDX-License-Identifier: BSD-3-Clause
+
+
+
 fruits = ["a","b","c","d","e" ]
 print("0番目から2番目の要素: ", fruits[0:3])
 print("2番目以降の要素: ", fruits[2:])
